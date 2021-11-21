bb8c661aa6e92de8c67d44d2135c04053d8807dc
Anish Nair <anishnair95@gmail.com>
2021-11-21 11:08:47 +0530

    added greatest function

--- multiply.cpp
+++ multiply.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
+
+int greatest(int a,int b){
+
+  return a>b?a:b;
+}
 int main(){
   int a=20;
   int b=30;
   int c=a*b;
   cout<<"Multiply of a and b"<<c;
+
+  cout<<"Largest of two number"<<greatest(a,b);
     return 0;
 }
