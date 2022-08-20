369d917c47be3c6aa9c6d878452610da731ed241
Anish Nair <anishnair95@gmail.com>
2022-08-20 13:11:49 +0530

    Added new change

--- arr.cpp
+++ arr.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-  cout<<"Changes in the branch- Fetch and sync using button" ;
+  cout<<"Changes in the branch - Fetch and sync using github" ;
   string s;
 
     return 0;
